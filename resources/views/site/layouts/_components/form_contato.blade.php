7c76921e48187a123e0195cc00012a6bc6731f43
devreissc <reis.rrsc@outlook.com>
2025-01-29 11:44:23 -0300

    foreach e erros

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -2,27 +2,45 @@
 <form action="{{ route('site.contato') }}" method="post">
     @csrf
     <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
+    @if($errors->has('nome'))
+        {{ $errors->first('nome') }}
+    @endif
     <br>
     <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
+    @if($errors->has('telefone'))
+        {{ $errors->first('telefone') }}
+    @endif
     <br>
     <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $classe }}">
+    @if($errors->has('email'))
+        {{ $errors->first('email') }}
+    @endif
     <br>
-    <select name="motivo_contato" class="{{ $classe }}">
+    <select name="motivo_contatos_id" class="{{ $classe }}">
         <option value="" >Qual o motivo do contato?</option>
 
         @foreach ($motivo_contato as $key => $motivo)
-            <option value="{{ $key }}" {{ old('motivo_contato') == $key ? 'selected' : '' }}>{{ $motivo }}</option>
+            <option value="{{ $motivo->id }}" {{ old('motivo_contato_id') == $motivo->id ? 'selected' : '' }}>{{ $motivo->motivo_contato }}</option>
         @endforeach
     </select>
+    @if($errors->has('motivo_contato_id'))
+        {{ $errors->first('motivo_contato_id') }}
+    @endif
     <br>
     <textarea name="mensagem"class="{{ $classe }}">{{ (old('mensagem') != '') ? old('mensagem') : 'Preencha aqui a sua mensagem' }}
     </textarea>
+    @if($errors->has('mensagem'))
+        {{ $errors->first('mensagem') }}
+    @endif
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
 
-<div style="width: 100%; height: 200px; background-color: #f99; position: absolute; top: 0px; left: 0px;">
-    <pre>
-        {{ print_r($errors) }}
-    </pre>
-</div>
+@if($errors->any())
+    <div style="position: absolute; top: 0px; left: 0px; width: 100%; background-color: #f99;">
+        @foreach ($errors->all() as $erro)
+            {{ $erro }}
+            <br>
+        @endforeach
+    </div>
+@endif
