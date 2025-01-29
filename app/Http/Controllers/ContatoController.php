db6480a4c86fb3caac27ca0f1a55ec2e408375fe
devreissc <reis.rrsc@outlook.com>
2025-01-29 11:45:22 -0300

    Alterações no find e validações

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\SiteContato;
+use App\MotivoContato;
 
 class ContatoController extends Controller
 {
@@ -33,25 +34,38 @@ class ContatoController extends Controller
         // $contato3 = new SiteContato();
         // $contato3->create($request->all());
 
-        $motivo_contato = [
-            '1' => 'Dúvida',
-            '2' => 'Elogio',
-            '3' => 'Reclamação'
-        ];
+        $motivo_contato = MotivoContato::all();
         
         return view('site.contato', ['titulo' => 'Contato', 'motivo_contato' => $motivo_contato]);
     }
 
     public function salvar(Request $request){
         // realiza a validação dos dados
-        $request->validate([
-            'nome' => ['required', 'min:3', 'max:40'], // required = obrigatório, min = mínimo de caracteres, max = máximo de caracteres
-            'telefone' => ['required'],
-            'email' => ['email'],
-            'motivo_contato' => ['required', 'numeric'],
-            'mensagem' => ['required', 'min:5'],
-        ]);
+        $request->validate(
+            [
+                'nome' => ['required', 'min:3', 'max:40', 'unique:site_contatos'], // required = obrigatório, min = mínimo de caracteres, max = máximo de caracteres
+                'telefone' => ['required'],
+                'email' => ['email'],
+                // 'motivo_contato' => ['required', 'numeric'],
+                'motivo_contatos_id' => ['required', 'numeric'],
+                'mensagem' => ['required', 'min:5'],
+            ],
+            [
+                // 'nome.required' => 'O campo nome precisa ser preenchido',
+                'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+                'nome.max' => 'O campo nome precisa ter no máximo 40 caracteres',
+                'nome.unique' => 'O nome informado já está em uso',
+                // 'telefone.required' => 'O campo telefone precisa ser preenchido',
+                'email.email' => 'O email informado não é válido',
+                // 'mensagem.required' => 'O campo mensagem precisa ser preenchido',
+                'mensagem.min' => 'O campo mensagem precisa ter no mínimo 5 caracteres',
+                // 'motivo_contatos_id.required' => 'O campo motivo de contato precisa ser preenchido',
+                'motivo_contatos_id.numeric' => 'O campo motivo de contato precisa ser um número',
+                'required' => 'O campo :attribute precisa ser preenchido', //   
+            ]
+        );
 
-        // SiteContato::create($request->all());
+        SiteContato::create($request->all());
+        return redirect()->route('site.index');
     }
 }
